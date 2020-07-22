bf0f4c82b42bba062f6a45a131eef53034674153
York <sc17yxm@leeds.ac.uk>
2020-07-22 21:56:55 +0100

    Added the attack system and death and win messages. Formatted some text

--- BattleScripts/main.py
+++ BattleScripts/main.py
@@ -16,9 +16,40 @@ print(bcolours.FAIL + bcolours.BOLD + "An enemy attacks!" + bcolours.ENDC)
 while running:
     print('=============================')
     player.chooseAction()
-    choice = input("Choose action:")
+    choice = input("Choose action: ")
     index = int(choice) - 1
 
-    print("You choose", index)
+    if index == 0:
+        dmg = player.genDmg()
+        enemy.takeDmg(dmg)
+        print ('You attacked for ',dmg, 'points of damage.')
+    elif index == 1:
+        player.chooseMagic()
+        magicChoice = int(input("Choose your spell: "))-1
+        magicDmg = player.genSpellDmg(magicChoice)
+        spell = player.getSpellName(magicChoice)
+        cost = player.getSpellCost(magicChoice)
+        if player.reduceMp(cost):
+            enemy.takeDmg(magicDmg)
+            print(bcolours.OKBLUE + "\n" + spell, "deals", str(magicDmg), "points of damage" + bcolours.ENDC)
+        else:
+            print(bcolours.FAIL + "\nNot enough MP\n" + bcolours.ENDC)
+            continue
 
-    running = False
+    enemyChoice = 1
+
+    enemyDmg = enemy.genDmg()
+    player.takeDmg(enemyDmg)
+    print('Enemy attacks for', enemyDmg)
+
+    print('=============================')
+    print('Enemy HP:', bcolours.FAIL + str(enemy.getHp()) + "/" + str(enemy.getMaxhp()) + bcolours.ENDC)
+    print('Player HP:', bcolours.OKGREEN + str(player.getHp()) + "/" + str(player.getMaxhp()) + bcolours.ENDC)
+    print('Player MP:', bcolours.OKBLUE + str(player.getMp()) + "/" + str(player.getMaxmp()) + bcolours.ENDC)
+
+    if enemy.getHp() == 0:
+        print(bcolours.OKGREEN + 'You win!' + bcolours.ENDC)
+        running = False
+    elif player.getHp() == 0:
+        print(bcolours.FAIL + 'You have been defeated!' + bcolours.ENDC)
+        running = False
