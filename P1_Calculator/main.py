130258848914f0094fa6a9255ecdd4c661f13e6b
York <sc17yxm@leeds.ac.uk>
2020-07-22 17:02:09 +0100

    Finished the program with a useable calculator

--- P1_Calculator/main.py
+++ P1_Calculator/main.py
@@ -8,13 +8,26 @@ run = True
 
 def performMath():
     global run
+    global previous
+    equation = ""
 
-    equation = input("Enter equation: ")
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation = input(str(previous))
+
     if equation == "quit":
+        print('Goodbye')
         run = False
     else:
-        previous = eval(equation)
-        print ("You typed", previous)
+        equation = re.sub('[a-zA-Z,.:()" "]','',equation)
+
+        if previous == 0:
+            previous = eval(equation)
+
+        else:
+            previous = eval(str(previous)+equation)
+
 
 while run:
   performMath()
