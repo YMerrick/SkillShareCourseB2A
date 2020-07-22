297e4e60714eaf79ed66219730e94ef1fbad92b3
York <sc17yxm@leeds.ac.uk>
2020-07-22 21:15:14 +0100

    Added the repeating loop

--- BattleScripts/main.py
+++ BattleScripts/main.py
@@ -2,7 +2,16 @@ from Classes.game import Person, bcolours
 
 
 magic = [{"name": "Fire","cost": 10,"dmg": 80},
-         {"name": "Thunder","cost": 10,"dmg": 90},
+         {"name": "Thunder","cost": 12,"dmg": 90},
          {"name": "Blizzard","cost": 10,"dmg": 60},]
 
 player = Person(400, 65, 60, 34, magic)
+enemy = Person(1200, 65, 45, 25, magic)
+
+running = True
+i = 0
+
+print(bcolours.FAIL + bcolours.BOLD + "An enemy attacks!" + bcolours.ENDC)
+
+while running:
+    pass
