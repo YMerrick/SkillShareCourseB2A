92d6eaaeb2f219fd829c0bc87671193a7ea48760
York <sc17yxm@leeds.ac.uk>
2020-07-23 19:54:11 +0100

    Downloaded some images using the new requests and pillow librares

--- Reading and writing files/requestsTests.py
+++ Reading and writing files/requestsTests.py
@@ -1,9 +1,16 @@
 import requests
+from PIL  import Image
+from io import BytesIO
 
+r = requests.get("https://i.pinimg.com/originals/a0/f4/33/a0f433603c8a62e9df3d7d25d3b3649e.png")
+print('Status code:', r.status_code)
+image = Image.open(BytesIO(r.content))
 
+path = "./image1."+image.format
 
-params = {"q":"pizza"}
-r = requests.get("http://bing.com/search", params=params)
-print(r.url)
-f = open("./page.html","w+")
-f.write(r.text)
+print(image.size,image.format,image.mode)
+
+try:
+    image.save(path,image.format)
+except IOError:
+    print('Can not save image')
