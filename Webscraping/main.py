d12dca4a95301e1a406b4362092e44e99bb7a206
York <sc17yxm@leeds.ac.uk>
2020-07-23 21:17:12 +0100

    created a simple image scraper for bing

--- Webscraping/main.py
+++ Webscraping/main.py
@@ -11,9 +11,13 @@ results = soup.find("ol",{"id":"b_results"})
 links = results.findAll("li",{"class": "b_algo"})
 
 for item in links:
-    item_text = item.find("a").text 
+    item_text = item.find("a").text
     item_href = item.find("a").attrs["href"]
 
     if item_text and item_href:
         print (item_text)
         print (item_href)
+        print('Summary:', item.find("a").parent.parent.find("p").text)
+
+        children = item.find("h2")
+        print('Next sibling of h2 is:', children.next_sibling)
