1d9dc3dd18f030b47742bee3bceeb5e89a7ec7f9
York <sc17yxm@leeds.ac.uk>
2020-07-23 19:42:34 +0100

    managed to pull a page from a query

--- Reading and writing files/requestsTests.py
+++ Reading and writing files/requestsTests.py
@@ -1,6 +1,9 @@
 import requests
 
-r = requests.get("http://google.com
 
+
+params = {"q":"pizza"}
+r = requests.get("http://bing.com/search", params=params)
+print(r.url)
 f = open("./page.html","w+")
 f.write(r.text)
