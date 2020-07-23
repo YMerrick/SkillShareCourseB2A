7079cc410e835d8420c928f8d6e23313083b190f
York <sc17yxm@leeds.ac.uk>
2020-07-23 18:09:45 +0100

    Creating and dumping files using json

--- Reading and writing files/main.py
+++ Reading and writing files/main.py
@@ -1,5 +1,17 @@
-newfile = open('newfile.txt','w+')
+import simplejson as json
+import os
 
-string = 'This is going to be written to the new text file'
+if os.path.isfile("./ages.json") and os.stat("./ages.json").st_size != 0:
+    old_file = open("./ages.json", "r+")
+    data = json.loads(old_file.read())
+    print('Current age is', data["age"],'--- adding a year')
+    data["age"] = data["age"] + 1
+    print('New age is', data["age"])
 
-newfile.write(string)
+else:
+    old_file = open("./ages.json","w+")
+    data = {"name":"Stef","age":27}
+    print('No file found setting default age to', data["age"])
+
+old_file.seek(0)
+old_file.write(json.dumps(data))
