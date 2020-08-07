30b5816d002a69ec48a749885d38ce56ab20941f
York <sc17yxm@leeds.ac.uk>
2020-08-07 20:21:01 +0100

    Got the switch tabs and web pages working

--- PyQt5Project/WebBrowser/main.py
+++ PyQt5Project/WebBrowser/main.py
@@ -15,7 +15,6 @@ class AddressBar(QLineEdit):
         super().__init__()
 
     def mousePressEvent(self,e):
-        print(e)
         self.selectAll()
 
 class App(QFrame):
@@ -25,7 +24,8 @@ class App(QFrame):
         super().__init__()
         self.setWindowTitle("Scuffed Browser")
         self.createApp()
-        self.setBaseSize(1300,700)
+        self.setMinimumSize(960,540)
+        self.setBaseSize(1366,768)
 
 
     def createApp(self):
@@ -38,6 +38,7 @@ class App(QFrame):
         self.tabBar.tabBarClicked.connect(self.switchTab)
 
         self.tabBar.setCurrentIndex(0)
+        self.tabBar.setDrawBase(False)
 
         #Keep track of tabs
         self.tabCount = 0
@@ -47,12 +48,23 @@ class App(QFrame):
         self.toolBar = QWidget()
         self.toolBarLayout = QHBoxLayout()
         self.addressBar = AddressBar()
+        self.addTabButton = QPushButton("+")
 
+        self.addressBar.returnPressed.connect(self.browseTo)
+
+        #Build Toolbar buttons
+        self.backButton = QPushButton("<-")
+        self.forwardButton = QPushButton("->")
+
+        self.backButton.connect(self.goBack)
+        self.forwardButton.connect(self.goForward)
+
+
+        #Build Toolbar
         self.toolBar.setLayout(self.toolBarLayout)
         self.toolBarLayout.addWidget(self.addressBar)
 
         #New tab button
-        self.addTabButton = QPushButton("+")
         self.addTabButton.clicked.connect(self.addTab)
 
         self.toolBarLayout.addWidget(self.addTabButton)
@@ -85,6 +97,9 @@ class App(QFrame):
         self.tabs[i].content = QWebEngineView()
         self.tabs[i].content.load(QUrl.fromUserInput("https://google.com"))
 
+        self.tabs[i].content.titleChanged.connect(lambda: self.tabContentChange(i,"title"))
+        self.tabs[i].content.iconChanged.connect(lambda: self.tabContentChange(i,"icon"))
+
         #Add webview to tabs layout
         self.tabs[i].layout.addWidget(self.tabs[i].content)
 
@@ -97,16 +112,65 @@ class App(QFrame):
 
         #Set tab on top of the screen
         self.tabBar.addTab("New Tab")
-        self.tabBar.setTabData(i,"tab"+str(i))
+        self.tabBar.setTabData(i,{"object":"tab"+str(i),"initial":i})
         self.tabBar.setCurrentIndex(i)
 
         self.tabCount += 1
 
     def switchTab(self, i):
-        tab_Data = self.tabBar.tabData(i)
+        tab_Data = self.tabBar.tabData(i)["object"]
 
         tab_Content = self.findChild(QWidget, tab_Data)
         self.container.layout.setCurrentWidget(tab_Content)
+
+    def browseTo(self):
+        text = self.addressBar.text()
+
+        i = self.tabBar.currentIndex()
+        tab = self.tabBar.tabData(i)["object"]
+        view = self.findChild(QWidget, tab).content
+
+        if "http" not in text:
+            if "." not in text:
+                url = "https:google.com/#q=" + text
+            else:
+                url = "https://" + text
+        else:
+            url = text
+
+        view.load(QUrl.fromUserInput(url))
+
+        self.tabs[i].content.titleChanged.connect(lambda: self.tabContentChange(i,"title"))
+        self.tabs[i].content.iconChanged.connect(lambda: self.tabContentChange(i,"icon"))
+
+    def tabContentChange(self,i,type):
+        tabObjectName = self.tabs[i].objectName()
+
+        count = 0
+        running = True
+
+        while running:
+            tabDataName = self.tabBar.tabData(count)
+
+            if count >= 99:
+                running = False
+
+            if tabObjectName == tabDataName["object"]:
+                if type == "title":
+                    newTitle = self.findChild(QWidget,tabObjectName).content.title()
+                    self.tabBar.setTabText(count, newTitle)
+                elif type == "icon":
+                    newIcon = self.findChild(QWidget, tabObjectName).content.icon()
+                    self.tabBar.setTabIcon(count,newIcon)
+                running = False
+            else:
+                count += 1
+
+    def goBack(self):
+        pass
+
+    def goForward(self):
+        pass
 
 
 if __name__ == "__main__":
